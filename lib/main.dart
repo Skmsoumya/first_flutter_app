ffd25ca76c5098d0beba72a8dcd7df9980e2ec9f
Skmsoumya <soumyakishormishra@gmail.com>
2019-11-14 20:19:12 +0530

    Statefulwidgets: adding statefullness

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,28 @@ import "package:flutter/material.dart";
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return MyAppState();
+  }
+}
+
+class MyAppState extends State<MyApp> {
+  var questionIndex = 0;
+
   @override
   Widget build(BuildContext context) {
-    
+    var questions = [
+      "What's your favourite color?",
+      "What's your favourite animal?",
+    ];
+
     void answerQuestion() {
-      print("answered question!");
+      setState(() {
+        questionIndex = questionIndex + 1;
+      });
+      print(questionIndex);
     }
 
     return MaterialApp(
@@ -21,7 +37,7 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: [
-            Text("The Question!"),
+            Text(questions[questionIndex]),
             RaisedButton(
               child: Text("Answer 1"),
               onPressed: answerQuestion,
