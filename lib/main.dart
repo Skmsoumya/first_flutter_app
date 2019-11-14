a8c4193c1cf7b38217d0f1522f60aa2b73b152eb
Skmsoumya <soumyakishormishra@gmail.com>
2019-11-14 19:21:11 +0530

    Trying app Scafolding

--- lib/main.dart
+++ lib/main.dart
@@ -9,12 +9,35 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    
+    void answerQuestion() {
+      print("answered question!");
+    }
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("My first flutter app!"),
         ),
-        body: Text("This is my default text"),
+        body: Column(
+          children: [
+            Text("The Question!"),
+            RaisedButton(
+              child: Text("Answer 1"),
+              onPressed: answerQuestion,
+            ),
+            RaisedButton(
+              child: Text("Answer 2"),
+              onPressed: () => print("Answer 2 choosen!"),
+            ),
+            RaisedButton(
+              child: Text("Answer 3"),
+              onPressed: () {
+                print("Answer 3 choosen!");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
